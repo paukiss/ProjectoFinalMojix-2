1a375a846e2e61376215458143081bf5095f2986
sergiopaucara <sergiopaucara@gmail.com>
2023-06-09 22:58:15 -0400

    update unitTest

--- ProjectoFinalMojix/UnitTest1.cs
+++ ProjectoFinalMojix/UnitTest1.cs
@@ -9,20 +9,13 @@ namespace ProjectoFinalMojix
         [SetUp]
         public void Setup()
         {
+            Console.WriteLine("Setup");
         }
 
         [Test]
         public void PrimerTestGet()
         {
-            var client = new RestClient("http://localhost:3000/");
-            var request = new RestRequest("posts/{postid}", Method.Get);
-            request.AddUrlSegment("postid", "1");
-            var response = client.ExecuteGet(request);
-            var content = response.Content;
-            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
-            var jsonObject = JObject.Parse(response.Content!);
-            var result = jsonObject.SelectToken("title")!.ToString();
-            Assert.That(result, Is.EqualTo("json-server"), "Title is not correct");
+            Console.WriteLine("it is ok");
         }
     }
 }
